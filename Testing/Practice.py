220edec83b01b5e875b90ab12c74764195b9eed6
Doomer4567 <Bykertn04@gmail.com>
2024-02-23 10:43:54 -0600

    Added Practice.py for class

---
+++ Testing/Practice.py
@@ -0,0 +1,10 @@
+class Student:
+    def __init__(self, first_name: str, last_name: str, year: str, grade: float, credit_hours: float):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.year = year
+        self.grade = grade
+        self.credit_hours = credit_hours
+
+s1 = Student('Bob','Marley','Sophomore', 4.2, 400.)
+
