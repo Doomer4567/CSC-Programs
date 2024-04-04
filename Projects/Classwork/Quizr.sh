c3525cd00d7f3927d5b48bdaae3d36bfabc47b32
Doomer4567 <Bykertn04@gmail.com>
2024-04-04 15:51:05 -0500

    sadjasf

---
+++ Projects/Classwork/Quizr.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+strBaseUrl="http://gutendex.com/books/?ids="
+URLarray=curl "$strBaseUrl"
+URLL="$urlArray" | jq 'Length'
+while [ x -lt $URLL ];
+do
+    Title="$URLarray" | jq -r ".[$x].title
+    mkdir "$Title"
+    ((x++))
+done
