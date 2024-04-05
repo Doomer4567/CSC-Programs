f434865450f50d788253687f17b6400e48fda812
Doomer4567 <Bykertn04@gmail.com>
2024-04-05 17:08:15 -0500

    LADIDIDA

--- Projects/Classwork/Quizr.sh
+++ Projects/Classwork/Quizr.sh
@@ -1,15 +1,21 @@
 #! /bin/bash
-
-strBaseUrl="http://gutendex.com/books/?ids=12,1,14,15"
-URLarray="curl $strBaseUrl"
-URLL=$(echo "$URLarray" | jq 'length')
-count=0
-while [ "$count" -lt "$URLL" ];
+mkdir Books
+cd Books || exit
+counter=0
+while [ "$counter" -lt "$1" ];
 do
-    urg=$(echo "$URLarray" | jq -r .["$x"].title)
-    Title=$(echo "$urg" | tr " " _)
-    mkdir Test/"$Title"
-    ((x++))
+    read -rp "Please enter a book id here: " UsrInput
+    URL="http://gutendex.com/books/?ids="
+    URLarray="$URL$UsrInput"
+    Download=$(curl "${URLarray})" | jq -r .results[0].download_count)
+    Time=$(date +"%Y-%m-%d")
+    ID=$(curl "${URLarray}" | jq -r .results[0].id)
+    mkdir "$ID"
+    cd "$ID" || exit
+    Title=$(curl "${URLarray}" | jq -r .results[0].title)
+    touch "$Title.txt"
+    echo "$Time:$Download" >> "$Title.txt"
+    for f in *\ *; do mv "$f" "${f// /_}"; done
+    cd ..
+    ((counter++))
 done
-
-ls -o
