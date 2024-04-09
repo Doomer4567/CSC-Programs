bb2e123a551e56d349b5aae364f5e01ca43121ea
Doomer4567 <Bykertn04@gmail.com>
2024-04-09 15:41:01 -0500

    WIIIS
    LAIFJ

--- Projects/Classwork/Quizr.sh
+++ Projects/Classwork/Quizr.sh
@@ -4,16 +4,15 @@ cd Books || exit
 counter=0
 while [ "$counter" -lt "$1" ];
 do
-    read -rp "Please enter a book id here: " UsrInput
+    read -rp "Please enter a book id here: " ID
     URL="http://gutendex.com/books/?ids="
-    URLarray="$URL$UsrInput"
-    Download=$(curl "${URLarray})" | jq -r .results[0].download_count)
-    Time=$(date +"%Y-%m-%d")
-    ID=$(curl "${URLarray}" | jq -r .results[0].id)
+    URLarray="$URL$ID"
     mkdir "$ID"
     cd "$ID" || exit
     Title=$(curl "${URLarray}" | jq -r .results[0].title)
     touch "$Title.txt"
+    Download=$(curl "${URLarray}" | jq .results[0].download_count)
+    Time=$(date +%Y-%m-%d)
     echo "$Time:$Download" >> "$Title.txt"
     for f in *\ *; do mv "$f" "${f// /_}"; done
     cd ..
