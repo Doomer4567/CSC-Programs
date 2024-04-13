6406395f6cf2f99c60adbd197a85b7c58a771d4c
Doomer4567 <Bykertn04@gmail.com>
2024-04-13 14:05:13 -0500

    Pot

---
+++ CSC/CSC2310/Lab05/SandWich.py
@@ -0,0 +1,72 @@
+
+class Sandwich:
+    def __init__(self, OrderNumber: int, BreadType: str, MeatType: str):
+        self.OrderNumber = OrderNumber
+        self.BreadType = BreadType
+        self.MeatType = MeatType
+        
+    @property 
+    def Topping(self) -> str:
+        return self.Topping
+    
+    @Topping.setter
+    def Topping(self, Toppings: int):
+        self.Toppings = Toppings
+        numToppings+=1
+    
+   
+    def set_Price(self):
+        Price = 3.75 + (0.50 * Toppings)
+        return Price
+
+    def get_OrderNumber(self):
+        return OrderNumber
+
+    def get_BreadType(self):
+        return BreadType
+
+    def get_MeatType(self):
+        return MeatType
+
+    def info(self):
+        print(OrderNumber)
+        print(BreadType)
+        print(MeatType)
+        print(Price)
+
+class Meal:
+    def __init__(self, Drink, Side):
+        self.Drink = Drink
+        self.Side = Side
+    
+    def get_Drink(self):
+        return Drink
+ 
+    def get_Side(self):
+        return Side
+
+    def get_Price(self):
+        Price = 6.75 + (0.50 * Toppings)
+        return Price
+
+    def info(self):
+        print(Drink)
+        print(Side)
+
+
+class KidsMeal:
+    def __init__(self, toy):
+        self.toy = toy
+
+    def get_toy(self):
+        return toy
+
+    def get_Price(self):
+        Price = 4.75 + (0.50 * x)
+        return Price
+
+    def info(self):
+        Sandwich.info()
+        print(toy)
+
+    
