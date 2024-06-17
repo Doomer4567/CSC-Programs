f1dd600f0a14ac0fe96146fe15dc03020d6dafab
Doomer4567 <Bykertn04@gmail.com>
2024-06-17 18:40:38 -0500

    Update Rust Projects

--- Projects/Rust/county_sorter/src/main.rs
+++ Projects/Rust/county_sorter/src/main.rs
@@ -1,8 +1,6 @@
 use std::fs::File;
 
 
-
 fn main() {
-    let mut file = File::open("counties_list.csv")
-    println!(File)
+    println!("Hello, World!");
 }
---
+++ Projects/Rust/county_sorter/target/debug/deps/County_Sorter-c5a78e5e48b30009.d
@@ -0,0 +1,5 @@
+/mnt/75e048c6-785f-42af-a864-177020086583/Git/CSC-Programs/Projects/Rust/county_sorter/target/debug/deps/County_Sorter-c5a78e5e48b30009: src/main.rs
+
+/mnt/75e048c6-785f-42af-a864-177020086583/Git/CSC-Programs/Projects/Rust/county_sorter/target/debug/deps/County_Sorter-c5a78e5e48b30009.d: src/main.rs
+
+src/main.rs:
