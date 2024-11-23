8e50dcc259ef1e11c74a7b3bf33ef4c234f38468
Doomer4567 <Bykertn04@gmail.com>
2024-11-23 02:04:09 -0600

    good progress

--- CSC/CSC2400/Program4/ejbyker42.cpp
+++ CSC/CSC2400/Program4/ejbyker42.cpp
@@ -2,48 +2,19 @@
 #include <fstream>
 #include <vector>
 
+
 //Function to find the max element in a vector
-int find_max_element(std::vector<int> INTARRAY, int SIZE){
-    //Debug Statement
-    //std::cout << "Got Here" << std::endl;
-    int temp = 0;
-    int j = 0;
-    for(int i = 1;SIZE-1;i++){
-        temp = INTARRAY[i];
-        j = i-1;
-        while(j >= 0 && INTARRAY[j] > temp){
-            INTARRAY[j+1] = INTARRAY[j];
-            j = j-1;
-        }
-        INTARRAY[j+1] = temp;
-    }
-    //Debug Statement
-    std::cout << "Got Here" << std::endl;
-    return INTARRAY.back();
-    
-    /*int max = 0;
-    if(SIZE == 0){
-        max = INTARRAY[0];
-        return max;
-    }
-    //determines if the back of the vector is bigger than the one in front of the element
-    if(INTARRAY[SIZE] > INTARRAY[SIZE-1]){
-        int temp = INTARRAY[SIZE];
-        INTARRAY[SIZE] = INTARRAY[SIZE-1];
-        INTARRAY[SIZE] = temp;
+int make_max_value(std::vector<int> INTARRAY, int back, int front){
+    int Num = 0;
+    if(!(front & 1 << INTARRAY.front())){
         INTARRAY.pop_back();
-        find_max_element(INTARRAY,SIZE-1);
-        return max;
-        
-    } // if the element is smaller than the one in front of it, it pops the element off the back of the vector
-    else if(INTARRAY[SIZE] <= INTARRAY[SIZE-1]){
-        INTARRAY.pop_back();
-        find_max_element(INTARRAY,SIZE-1);
-        return max;
+        Num = Num + make_max_value(INTARRAY,back,front);
     }
-    */
-    return 0;
-} 
+    if(!(back & 1 << INTARRAY.back())){
+        return INTARRAY.back();
+    }
+    return Num + INTARRAY.back();
+}
 
 int main(int argc, char* argv[]){
     //Opens and checks if an argument is given and if the file given opens
@@ -58,24 +29,33 @@ int main(int argc, char* argv[]){
     }
 
     //makes a array to hold integers from file
-    int SIZE = 0;
     std::vector<int> INTARRAY;
+    int max = 0;
     while(!fs.eof()){
-        int tempint;
-        fs >> tempint;
-        INTARRAY.push_back(tempint);
-        SIZE++;
+        char tempint;
+        fs.get(tempint);
+        INTARRAY.push_back(int(tempint));
+        if(fs.peek() == '\n'){
+            int back = INTARRAY.back() << 1;
+            int front = INTARRAY.front() << 1;
+                //Debug Statement
+                //std::cout << "Got Here" << std::endl;
+            int tempmax = make_max_value(INTARRAY,back,front);
+                //Debug Statement
+                //std::cout << "Got Here" << std::endl;
+            if(max <= tempmax){
+                max = tempmax;
+            }
+            while(!INTARRAY.empty()){
+                INTARRAY.pop_back();
+            }
+        }
+        
     }
+
+    std::cout << max << std::endl;
     //Debug Statement
     //std::cout << "Got Here" << std::endl;
-    
-    int max = find_max_element(INTARRAY,SIZE);
-    if (max == 0){
-        std::cout << "Something went Wrong Please Try Again" << std::endl;
-    }
-    else{
-        std::cout << "The Max element is " << max << std::endl;
-    }
 
     //closes the file
     fs.close();
