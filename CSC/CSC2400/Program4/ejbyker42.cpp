527e7673ee2564f1f9808da40a03c8c5f7cc2044
Doomer4567 <Bykertn04@gmail.com>
2024-11-25 12:58:59 -0600

    heherea

--- CSC/CSC2400/Program4/ejbyker42.cpp
+++ CSC/CSC2400/Program4/ejbyker42.cpp
@@ -1,63 +1,68 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <stack>
 
 
 //Function to find the max element in a vector
-int make_max_value(std::vector<int> INTARRAY, int back, int front){
-    int Num = 0;
-    if(!(front & 1 << INTARRAY.front())){
-        INTARRAY.pop_back();
-        Num = Num + make_max_value(INTARRAY,back,front);
+int make_max_value(std::vector<int> INTARRAY, int Iterator){
+    std::stack<int> intst;
+    int max = 0;
+    bool sofe = 0;
+    //checks if the function reached the front of the array
+
+    try{
+        if(INTARRAY[Iterator] != INTARRAY.front()){
+        max = make_max_value(INTARRAY,Iterator-1);
     }
-    if(!(back & 1 << INTARRAY.back())){
-        return INTARRAY.back();
+    } catch (const std:: overflow_error){
+        sofe = 1;
     }
-    return Num + INTARRAY.back();
+
+    if(sofe == 1){
+        while
+    }
+    //if max is less than whhere we are in the array change max
+    if(max < INTARRAY[Iterator]){
+        max = INTARRAY[Iterator];
+        return max;
+    }   
+
+    return max;
 }
 
 int main(int argc, char* argv[]){
+    std::stack<int> intst;
     //Opens and checks if an argument is given and if the file given opens
     std::fstream fs;
-     if(argc > 1){
-    std::string arg1(argv[1]);
-    fs.open(arg1);
+    if(argc > 1){
+        std::string arg1(argv[1]);
+        fs.open(arg1); 
     }
     if(fs.fail()){
         std::cout << "File Failed to Open!" << std::endl;
         exit(0);
     }
+   //fs.open("max-element-10.txt");
+
+    
+    
 
     //makes a array to hold integers from file
     std::vector<int> INTARRAY;
-    int max = 0;
-    while(!fs.eof()){
-        char tempint;
-        fs.get(tempint);
-        INTARRAY.push_back(int(tempint));
-        if(fs.peek() == '\n'){
-            int back = INTARRAY.back() << 1;
-            int front = INTARRAY.front() << 1;
-                //Debug Statement
-                //std::cout << "Got Here" << std::endl;
-            int tempmax = make_max_value(INTARRAY,back,front);
-                //Debug Statement
-                //std::cout << "Got Here" << std::endl;
-            if(max <= tempmax){
-                max = tempmax;
-            }
-            while(!INTARRAY.empty()){
-                INTARRAY.pop_back();
-            }
-        }
+    int num;
+    while (!fs.eof())
+    {
+        fs >> num;
+        INTARRAY.push_back(num);
         
     }
-
-    std::cout << max << std::endl;
+    num = INTARRAY.size();
+    std::cout << make_max_value(INTARRAY,num) << std::endl;
     //Debug Statement
     //std::cout << "Got Here" << std::endl;
 
     //closes the file
     fs.close();
-
+    return 0;
 }
