f46288a084465acd3442ac5e47851f0d3602f79f
Doomer4567 <Bykertn04@gmail.com>
2024-11-25 13:57:57 -0600

    sad :(

--- CSC/CSC2400/Program4/ejbyker42.cpp
+++ CSC/CSC2400/Program4/ejbyker42.cpp
@@ -9,32 +9,10 @@ int make_max_value(std::vector<int> INTARRAY, int Iterator, bool &sofe){
     std::stack<int> intst;
     int max = 0;
     //checks if the function reached the front of the array
-    //if the function reaches either the memory limit of the computer
-    //or the safe memory limit a device, text editor, or ide gives
-    //the base case will catch that and set sofe(StackOverFlowError to ON)
-    try{
-        if(INTARRAY[Iterator] != INTARRAY.front()){
+    if(INTARRAY[Iterator] != INTARRAY.front()){
         max = make_max_value(INTARRAY,Iterator-1,sofe);
     }
-    } catch (const std:: overflow_error){
-        sofe = 1;
-    }
 
-    //As long as the error checker is ON,
-    //the following lines will do as follows
-    //1. save where in the stack the error happened
-    //2. clear out all stacks to free up memory
-    //3. set the iterator to the value held from when the error was thrown
-    //4. set the error checker to Off
-    //5. continue running the function
-    if(sofe == 1){
-        int holditerator = intst.size();
-        while(!intst.empty()){
-            return max;
-        }
-        Iterator = holditerator;
-        sofe == 0;
-    }
     //if max is less than whhere we are in the array change max
     if(max < INTARRAY[Iterator]){
         max = INTARRAY[Iterator];
