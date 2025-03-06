d7ce0a4b6523e4271edd4d20fad7916b2e31b525
Doomer4567 <Bykertn04@gmail.com>
2025-03-06 08:47:45 -0600

    update

---
+++ CSC/CSC4575/DES_Encryption/Main.java
@@ -0,0 +1,7 @@
+package DES_Encryption;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
--- Projects/Java/Relearn_Java/Main.java
+++ Projects/Java/Relearn_Java/Main.java
@@ -1,9 +1,8 @@
 package Projects.Java.Relearn_Java;
 import java.util.Scanner;
 
-/**
- * Main
- */
+
+// Main Class
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
